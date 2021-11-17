1d74934095f19ed2a66e731e8f6f000f3c3b7603
wilmar suarez <wilmarsuarez@hotmail.com>
2021-11-16 20:54:45 -0500

    mi app16/11

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+  ngOnInit(){
+    let numero=20;
+  
+    numero=10;
+    numero=numero+15;
+    numero =3;
+    console.log(numero);
 
+
+
+  }
 }
